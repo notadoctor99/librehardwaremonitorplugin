9e810809d4e282cf74fa5e9be24e5644bf876ef6
NotADoctor99 <not.a.doctor.b99@gmail.com>
2023-03-28 14:36:54 +0300

    LibreHardwareMonitor removed from plugin installer

--- src/LibreHardwareMonitorPlugin/BuildEvents/BuildPackage.cmd
+++ src/LibreHardwareMonitorPlugin/BuildEvents/BuildPackage.cmd
@@ -4,6 +4,5 @@ set PROJECT_DIR=%~1
 set OUTPUT_DIR=%~2
 
 xcopy /s /y %PROJECT_DIR%metadata\ %OUTPUT_DIR%metadata\
-xcopy /s /y %PROJECT_DIR%..\LibreHardwareMonitor\ %OUTPUT_DIR%LibreHardwareMonitor\
 
 %PROJECT_DIR%LoupedeckPluginTool\LoupedeckPluginTool.exe pack -input=%OUTPUT_DIR% -output=%OUTPUT_DIR%..\LibreHardwareMonitor.lplug4
